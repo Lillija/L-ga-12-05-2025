c8dd4e4160cf17af7e1b5513561f3fa6b5cff51b
Your Name <youremail@example.com>
2025-05-12 10:28:14 +0300

    First commit

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/test', function () {
-//     // return view('welcome');
 
-// });
-
-// Route::get('/test', function (){
-//     echo "<h1>test</h1>";
-//     $name = "Renārs";
-//     echo $name;
-// });
 Route::get('/test', function (){
     return view('test', ['variable' => "my variable text value"]);
 });
